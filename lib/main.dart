74b353bbd7c0d9b5062e1bf16207ec23d4c10c13
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2024-10-31 11:08:50 -0400

    temporary setup with both tabbar and navbar

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,53 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: CupertinoPageScaffold(
-        // const Navigation bar for now to fix display
-        navigationBar: const CupertinoNavigationBar(
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-          leading: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "indulge",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold
-                ),
+      home: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.star_fill),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.star_fill),
+              label: 'Favorites',
+            ),
+          ],
+        ), 
+        tabBuilder: (BuildContext context, int index) {
+          return CupertinoPageScaffold(
+            navigationBar: const CupertinoNavigationBar(
+              backgroundColor: Color.fromARGB(255, 255, 255, 255),
+              leading: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "indulge",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-          //just text for now
-          trailing: Text("+ 20 mi -"),
-        ),
-        child: Center(
-          child: Container(
-            padding: EdgeInsets.all(8.0),
-            child: UserListsView(),
-          ),
-        ),
+              //just text for now
+              trailing: Text("+ 20 mi -"),
+            ),
+            child: CupertinoTabView(
+              builder: (BuildContext context) {
+                return Center(
+                  child: Center(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: UserListsView(),
+                    ),
+                  ),
+                );
+              },
+            )
+          );
+        },
       ),
     );
   }
