a5beca24fefcfee27a9c5faf751d4df2003cb0eb
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2024-11-01 12:53:15 -0400

    moved MainPage to separate widget that is Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -45,55 +45,68 @@ class MainApp extends StatelessWidget {
           //just text for now
           trailing: Text("+ 20 mi -"),
         ),
-        child: CupertinoTabScaffold(
-          backgroundColor: CupertinoColors.white,
-          tabBar: CupertinoTabBar(
-            backgroundColor: CupertinoColors.white,
-            activeColor: CupertinoColors.black,
-            inactiveColor: CupertinoColors.inactiveGray,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.home),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.bars),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.pencil),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.profile_circled),
-              ),
-            ],
-          ), 
-          tabBuilder: (BuildContext context, int index) {
-            return CupertinoTabView(
-              builder: (BuildContext context) {
-                return CupertinoPageScaffold(
-                  backgroundColor: CupertinoColors.transparent,
-                  child: Navigator(
-                    onGenerateRoute: (RouteSettings settings) {
-                      switch (index) {
-                        case 0:
-                          return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
-                        case 1:
-                          return local_router.Router.generateRoute(const RouteSettings(name: listRoute));
-                        case 2:
-                          return local_router.Router.generateRoute(const RouteSettings(name: reviewRoute));
-                        case 3:
-                          return local_router.Router.generateRoute(const RouteSettings(name: profileRoute));
-                        default:
-                          // default to home if invalid tab index
-                          return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
-                      }
-                    },
-                  )
-                );
-              },
+        child: MainPage()
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      backgroundColor: CupertinoColors.white,
+      tabBar: CupertinoTabBar(
+        backgroundColor: CupertinoColors.white,
+        activeColor: CupertinoColors.black,
+        inactiveColor: CupertinoColors.inactiveGray,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.bars),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.pencil),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.profile_circled),
+          ),
+        ],
+      ), 
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              backgroundColor: CupertinoColors.transparent,
+              child: Navigator(
+                onGenerateRoute: (RouteSettings settings) {
+                  switch (index) {
+                    case 0:
+                      return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
+                    case 1:
+                      return local_router.Router.generateRoute(const RouteSettings(name: listRoute));
+                    case 2:
+                      return local_router.Router.generateRoute(const RouteSettings(name: reviewRoute));
+                    case 3:
+                      return local_router.Router.generateRoute(const RouteSettings(name: profileRoute));
+                    default:
+                      // default to home if invalid tab index
+                      return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
+                  }
+                },
+              )
             );
           },
-        )
-      ),
+        );
+      },
     );
   }
 }
