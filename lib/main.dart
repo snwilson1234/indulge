9fe698e77a87d1349d7bb32c8f5b920c9e6041d4
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2024-11-01 10:02:26 -0400

    added working navigation and routes for bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-import 'package:indulge/lists/views/user_lists_view.dart';
+import 'package:indulge/routing/router.dart' as local_router;
+import 'package:indulge/routing/routes.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,164 +13,71 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
       home: CupertinoTabScaffold(
+        backgroundColor: CupertinoColors.white,
         tabBar: CupertinoTabBar(
+          backgroundColor: CupertinoColors.white,
+          activeColor: CupertinoColors.black,
+          inactiveColor: CupertinoColors.inactiveGray,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.star_fill),
-              label: 'Home',
+              icon: Icon(CupertinoIcons.home),
             ),
             BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.star_fill),
-              label: 'Favorites',
+              icon: Icon(CupertinoIcons.bars),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.pencil),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.profile_circled),
             ),
           ],
         ), 
         tabBuilder: (BuildContext context, int index) {
-          return CupertinoPageScaffold(
-            navigationBar: const CupertinoNavigationBar(
-              backgroundColor: Color.fromARGB(255, 255, 255, 255),
-              leading: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "indulge",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold
-                    ),
+          return CupertinoTabView(
+            builder: (BuildContext context) {
+              return CupertinoPageScaffold(
+                backgroundColor: CupertinoColors.transparent,
+                navigationBar: const CupertinoNavigationBar(
+                  backgroundColor: CupertinoColors.white,
+                  leading: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "indulge",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              //just text for now
-              trailing: Text("+ 20 mi -"),
-            ),
-            child: CupertinoTabView(
-              builder: (BuildContext context) {
-                return Center(
-                  child: Center(
-                    child: Container(
-                      padding: EdgeInsets.all(8.0),
-                      child: UserListsView(),
-                    ),
-                  ),
-                );
-              },
-            )
+                  //just text for now
+                  trailing: Text("+ 20 mi -"),
+                ),
+                child: Navigator(
+                  onGenerateRoute: (RouteSettings settings) {
+                    switch (index) {
+                      case 0:
+                        return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
+                      case 1:
+                        return local_router.Router.generateRoute(const RouteSettings(name: listRoute));
+                      case 2:
+                        return local_router.Router.generateRoute(const RouteSettings(name: reviewRoute));
+                      case 3:
+                        return local_router.Router.generateRoute(const RouteSettings(name: profileRoute));
+                      default:
+                        // default to home if invalid tab index
+                        return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
+                    }
+                  },
+                )
+              );
+            },
           );
         },
-      ),
+      )
     );
   }
 }
-
-// class Login extends StatelessWidget {
-//   const Login({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             const Expanded(
-//               flex: 1,
-//               child: Text("Login")
-//             ),
-//             const Expanded(
-//               flex: 2,
-//               child: LoginForm(),
-//             ),
-//             Expanded(
-//               flex: 1,
-//               child: Column(
-//                 children: [
-//                   const Text("Don't have an account with us? Sign up below."),
-//                   OutlinedButton(
-//                     onPressed: () {
-//                       print("pressed Sign Up");
-//                     }, 
-//                     child: const Text("Sign Up")
-//                   ),
-//                 ],
-//               )
-//             ),
-//           ],
-//       ),
-//       )
-//     );
-//   }
-// }
-
-// class LoginForm extends StatefulWidget {
-//   const LoginForm({super.key});
-
-//   @override
-//   LoginFormState createState() {
-//     return LoginFormState();
-//   }
-// }
-
-// class LoginFormState extends State<LoginForm> {
-//   // global key that uniquely identifies the Form widget
-//   // and allows validation
-//   final _formKey = GlobalKey<FormState>();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Form(
-//       key: _formKey,
-//       child: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           children: [
-//             // USERNAME FIELD
-//             ConstrainedBox(
-//               constraints: BoxConstraints.loose(
-//                 const Size(300, 50)
-//               ),
-//               child: TextFormField(
-//                 decoration: const InputDecoration(
-//                 icon: Icon(Icons.person),
-//                 hintText: 'Enter your username here.',
-//                 labelText: 'Username *',
-                
-//               ),
-//                 validator: (String? value) {
-//                   return (value != null) ? 'Required field' : null;
-//                 }
-//               ),
-//             ),
-//             // PASSWORD FIELD
-//             ConstrainedBox(
-//               constraints: BoxConstraints.loose(
-//                 const Size(300, 50)
-//               ),
-//               child: TextFormField(
-//                   obscureText: true,
-//                   decoration: const InputDecoration(
-//                     icon: Icon(Icons.person),
-//                     hintText: 'Enter your password here.',
-//                     labelText: 'Password *',
-//                   ),
-//                   validator: (String? value) {
-//                     return (value != null) ? 'Required field' : null;
-//                   }
-//               ),
-//             ),
-//             OutlinedButton(
-//               style: OutlinedButton.styleFrom(
-//                 textStyle: const TextStyle(fontSize: 20)
-//               ),
-//               onPressed: () {
-//                 print("pressed login");
-//               }, 
-//               child: Text("Login")
-//             ),
-//           ],    
-//         ),
-//       ),
-//     );
-//   }
-// }
---
+++ lib/routing/routes.dart
@@ -0,0 +1,5 @@
+// add any new routes needed to this file
+const String homeRoute = '/';
+const String listRoute = '/lists';
+const String reviewRoute = '/reviews';
+const String profileRoute = '/profile';
