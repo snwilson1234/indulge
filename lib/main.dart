4e987198161620434911cf678069dd6488d148a2
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2024-11-05 10:14:03 -0500

    making same changes to nav as i did in review detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+// Flutter packages
 import 'package:flutter/cupertino.dart';
 import 'package:indulge/routing/router.dart' as local_router;
 import 'package:indulge/routing/routes.dart';
+
+// Our views
+import 'package:indulge/lists/views/user_lists_view.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -12,72 +18,71 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: CupertinoTabScaffold(
-        backgroundColor: CupertinoColors.white,
-        tabBar: CupertinoTabBar(
+      initialRoute: homeRoute,
+      onGenerateRoute: local_router.Router.generateRoute,
+      home: CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
           backgroundColor: CupertinoColors.white,
-          activeColor: CupertinoColors.black,
-          inactiveColor: CupertinoColors.inactiveGray,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.bars),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.pencil),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.profile_circled),
-            ),
-          ],
-        ), 
-        tabBuilder: (BuildContext context, int index) {
-          return CupertinoTabView(
-            builder: (BuildContext context) {
-              return CupertinoPageScaffold(
-                backgroundColor: CupertinoColors.transparent,
-                navigationBar: const CupertinoNavigationBar(
-                  backgroundColor: CupertinoColors.white,
-                  leading: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "indulge",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
-                  //just text for now
-                  trailing: Text("+ 20 mi -"),
+          leading: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "indulge",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
                 ),
-                child: Navigator(
-                  onGenerateRoute: (RouteSettings settings) {
-                    switch (index) {
-                      case 0:
-                        return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
-                      case 1:
-                        return local_router.Router.generateRoute(const RouteSettings(name: listRoute));
-                      case 2:
-                        return local_router.Router.generateRoute(const RouteSettings(name: reviewRoute));
-                      case 3:
-                        return local_router.Router.generateRoute(const RouteSettings(name: profileRoute));
-                      default:
-                        // default to home if invalid tab index
-                        return local_router.Router.generateRoute(const RouteSettings(name: homeRoute));
-                    }
-                  },
-                )
-              );
-            },
-          );
-        },
-      )
+              ),
+            ],
+          ),
+          //just text for now
+          trailing: Text("+ 20 mi -"),
+        ),
+        child: MainPage()
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      backgroundColor: CupertinoColors.white,
+      tabBar: CupertinoTabBar(
+        backgroundColor: CupertinoColors.white,
+        activeColor: CupertinoColors.black,
+        inactiveColor: CupertinoColors.inactiveGray,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.bars),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.pencil),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.profile_circled),
+          ),
+        ],
+      ), 
+      tabBuilder: (BuildContext context, int index) {
+        return const <Widget>[
+          CupertinoPageScaffold(child:Text("placeholder for home")),
+          CupertinoPageScaffold(child:UserListsView()),
+          CupertinoPageScaffold(child:Text("placeholder for home")),
+          CupertinoPageScaffold(child:Text("placeholder for profile"))
+        ][index];
+      },
     );
   }
 }
